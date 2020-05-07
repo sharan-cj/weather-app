7caf0b1c343e23185350bb00ff6f75ca968e6eaf
sharan-cj <sharan.cj.gowda@gmail.com>
2020-05-07 20:32:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import WeatherCard from "./WeatherCard/WeatherCard";
+import Axios from "axios";
 
 function App() {
+  const [cities, setCity] = useState({ loading:'' });
+  const [activeCity, setActiveCity ] = useState('');
+  useEffect(() => {
+    async function getCities() {
+      const result = await Axios(
+        "https://weather-app-f99b3.firebaseio.com/.json?auth=5IQ99oUxwNsgp5jzLlLPlJTuTrqV94FL8viwcTkY"
+      );
+      setCity(result.data);
+    }
+    getCities();
+  });
+  // console.log(cities);
+
+  
+  function clickHandler(city){
+    setActiveCity(city);
+    console.log(activeCity);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h3>The Weather App</h3>
+        <div className="city">
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Select a City
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              {Object.keys(cities).map((city) => {
+                return (
+                  <a className="dropdown-item" onClick={()=>clickHandler(city)} key={city} href='#'>
+                    {city}
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+      {activeCity ? <WeatherCard city={activeCity}/> : null}
+      
     </div>
   );
 }
---
+++ src/WeatherCard/WeatherCard.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from "react";
+import "./WeatherCard.css";
+import Axios from "axios";
+
+function WeatherCard({ city }) {
+ 
+    const [weatherData, setWeatherData] = useState({main: { temp: "", temp_max: "", temp_min: "", humidity: "" },weather: [{ main: "" }], });
+    const [temperature, setTemperature] = useState(weatherData.main.temp);
+    const [maxTemperature, setMaxTemperature] = useState(weatherData.main.temp_max);
+    const [minTemperature, setMinTemperature] = useState(weatherData.main.temp_min);
+    const [isCelsiusActive, setCelsiusStatus] = useState(false);
+    const [isFahrenheitActive, setFahrenheitStatus] = useState(false);
+    const [isKelvinActive, setKelvinStatus] = useState(true);
+    const apiKey = 'bcc98771a9d310ebd603e73bb814a841';
+    
+    useEffect(() => {
+        async function getData() {
+            const response = await Axios.get(
+            `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+             );
+            console.log(response.data);
+            setWeatherData(response.data);
+         }
+        getData();
+        var temp = Number(weatherData.main.temp);
+        var maxTemp = Number(weatherData.main.temp_max);
+        var minTemp = Number(weatherData.main.temp_min);
+        
+        if(isKelvinActive){
+            setTemperature(weatherData.main.temp);
+            setMaxTemperature(weatherData.main.temp_max);
+            setMinTemperature(weatherData.main.temp_min);
+        }
+        
+        if(isCelsiusActive) {
+            temp = toCelsius(temp);
+            maxTemp = toCelsius(maxTemp);
+            minTemp = toCelsius(minTemp);
+            setTemperature(temp);
+            setMaxTemperature(maxTemp);
+            setMinTemperature(minTemp);
+        }
+
+        if(isFahrenheitActive){
+            temp = toFahrenheit(temp);
+            maxTemp = toFahrenheit(maxTemp);
+            minTemp = toFahrenheit(minTemp);
+            setTemperature(temp);
+            setMaxTemperature(maxTemp);
+            setMinTemperature(minTemp);
+        }
+    
+    });
+
+    
+    var temp = Number(weatherData.main.temp);
+    var maxTemp = Number(weatherData.main.temp_max);
+    var minTemp = Number(weatherData.main.temp_min);  
+    function clickHandler(event){
+        let currentTempUnit = event.target.name;
+            console.log(currentTempUnit);
+        if (currentTempUnit === 'celsius'){
+            temp = toCelsius(temp);
+            maxTemp = toCelsius(maxTemp);
+            minTemp = toCelsius(minTemp);
+            setTemperature(temp);
+            setMaxTemperature(maxTemp);
+            setMinTemperature(minTemp);
+            setFahrenheitStatus(false);
+            setKelvinStatus(false);
+            setCelsiusStatus(true);
+            // console.log(temp);
+        }
+        else if(currentTempUnit === 'fahrenheit'){
+            temp = toFahrenheit(temp);
+            maxTemp = toFahrenheit(maxTemp);
+            minTemp = toFahrenheit(minTemp);
+            setTemperature(temp);
+            setMaxTemperature(maxTemp);
+            setMinTemperature(minTemp);
+            setCelsiusStatus(false);
+            setKelvinStatus(false);
+            setFahrenheitStatus(true);
+            // console.log(temp);
+        }
+        else {
+            setTemperature(weatherData.main.temp);
+            setMaxTemperature(weatherData.main.temp_max);
+            setMinTemperature(weatherData.main.temp_min);
+            setFahrenheitStatus(false);
+            setCelsiusStatus(false);
+            setKelvinStatus(true);
+        }
+  };
+
+  function toCelsius(temp){
+        return (temp - 273.15).toFixed(1);
+  };
+
+  function toFahrenheit(temp){
+        return (((temp - 273.15)*1.8)+32).toFixed(1);
+  };
+
+  const displaySymbol = (<>
+        {isCelsiusActive ? <span> &deg;C</span> : null}
+        {isFahrenheitActive ? <span> &deg;F</span> : null}
+        {isKelvinActive ? <span> K</span>: null}
+        </>
+  );
+
+  return (
+    <div className="container">
+      <div className="city-name">{city}</div>
+      <div className="temperature">{temperature}{displaySymbol}</div>
+      <div className="weather">{weatherData.weather[0].main}</div>
+      <div className="max-min">
+      <span>Max : <span className='max'> {maxTemperature}{displaySymbol}</span></span>
+      <span>Min : <span className='min'> {minTemperature}{displaySymbol}</span></span>
+      </div>
+      <div className="temperature-unit">
+        view in : <button name='kelvin'onClick={(event)=>clickHandler(event)}>K</button> 
+                  <button name='fahrenheit'onClick={(event)=>clickHandler(event)}>&deg;F</button> 
+                  <button name='celsius'onClick={(event)=>clickHandler(event)}> &deg;C</button>  
+      </div>
+    </div>
+  );
+}
+
+export default WeatherCard;
